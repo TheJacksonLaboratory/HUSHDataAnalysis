aad107c1a9e07367aca05b9c119c3995216954b6
Aaron Zhang <kingmanzhang@gmail.com>
2018-05-14 17:44:19 -0400

    save R

--- src/main/r_script/accumulated_freq_count_obs.R
+++ src/main/r_script/accumulated_freq_count_obs.R
@@ -8,8 +8,8 @@ data <- read.csv("/Users/zhangx/git/HushToFhir/data/accumulated_cpt_count.csv", 
 ## total cpt count: 54683532
 ## create a cumulated count graph
 total_count <- 54683532
-ggplot(data) + geom_line(aes(x = step - 10, y = count / total_count), color = "blue") +
-  geom_point(aes(x = step -10, y = count / total_count), color = "red", shape = 1, size = 2) + 
+ggplot(data) + geom_line(aes(x = step, y = count / total_count), color = "blue") +
+  geom_point(aes(x = step, y = count / total_count), color = "red", shape = 1, size = 2) + 
   scale_y_continuous(labels = percent) +
   xlab("most frequent concepts") + ylab("cumulative freq (% of total)") + ggtitle("Cumulative freq of top 500 concepts") +
   theme_bw() + theme(panel.grid = element_blank())
