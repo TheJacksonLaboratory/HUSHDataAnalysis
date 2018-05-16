e5321f266308cb0311c9804a352daaa2feb0ed19
Aaron Zhang <kingmanzhang@gmail.com>
2018-05-16 17:08:38 -0400

    document

--- src/main/java/org/jax/App.java
+++ src/main/java/org/jax/App.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 public class App {
 
     private final static Logger logger = LoggerFactory.getLogger(App.class);
+    final static String WORKING_DIR = System.getProperty("user.dir");
 
     static void max_nums(String observationPath) {
 
@@ -67,17 +68,23 @@ public class App {
 
      static void concept_freq(String path) {
         //Queue<CPT_Component> priority_queue = new PriorityQueue<>();
+         //read in data, count how many time each concept occurred
         Map<String, Integer> cpt_map = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
+                if (line.startsWith("\"")) {
+                    line = line.substring(1, line.length());
+                }
+                if (line.endsWith("\"")) {
+                    line = line.substring(0, line.length() - 1);
+                }
                 if (cpt_map.containsKey(line)) {
                     cpt_map.put(line, cpt_map.get(line) + 1);
                 } else {
                     cpt_map.put(line, 1);
                 }
             }
-            reader.close();
         } catch (FileNotFoundException e) {
             logger.error("file not found: " + path);
 
@@ -97,21 +104,39 @@ public class App {
                 return o2.count - o1.count;
             }
         });
-        //calculate total counts
-        int total_count = list.stream().mapToInt(p -> p.count).sum();
-        //calculate how many times the top 100 concepts occurred
-        int top_100 = list.stream().limit(100).mapToInt(p -> p.count).sum();
 
-        //print out information
-        list.stream().limit(10).forEach(System.out::println);
-        //total count: 54683532
-        System.out.println("total count: " + total_count);
-        System.out.println("top 100 count: " + top_100);
-        System.out.println("Fraction of top 100 concept: " + (0.0 + top_100) / total_count);
-        //list.stream().limit(100).forEach(System.out::println);
+         //calculate total counts
+         int total_count = list.stream().mapToInt(p -> p.count).sum();
+         //calculate how many times the top 100 concepts occurred
+         int top_100 = list.stream().limit(100).mapToInt(p -> p.count).sum();
 
-         //frequency count histogram
-         //top 500, step size 20
+         //print out information
+         list.stream().limit(10).forEach(System.out::println);
+         //total count: 54683532
+         System.out.println("total count: " + total_count);
+         System.out.println("top 100 count: " + top_100);
+         System.out.println("Fraction of top 100 concept: " + (0.0 + top_100) / total_count);
+         //list.stream().limit(100).forEach(System.out::println);
+
+
+        //output the count per concept, from most frequent to lease frequent
+         String cpt_counts_path = WORKING_DIR + File.separator + "data" + File.separator + "cpt_counts.csv";
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(cpt_counts_path))) {
+             writer.write("# this table describes the count for each observation concept in OBSERVATION_FACTS.txt ##\n");
+             writer.write("cpt,count\n");
+             list.forEach(p -> {
+                 try {
+                     writer.write(p.name + "," + p.count + "\n");
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             });
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+
+         //output cumulative counts
          Stack<Integer> count_hist = new Stack<>();
          for (int i = 0; i < 500; i = i + 20) {
              int previous_sum = 0;
@@ -127,20 +152,93 @@ public class App {
              System.out.println(String.format("[%d - %d]: %d", i, i + 20, count_hist.peek()));
          }
 
-         try (BufferedWriter writer = new BufferedWriter(new FileWriter("data/accumulated_cpt_count.csv"))) {
+         String cpt_cumulative_counts_path = WORKING_DIR + File.separator + "data" + File.separator + "cumulative_cpt_count.csv";
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(cpt_cumulative_counts_path))) {
              //write out stack to a file for visualization
+             writer.write("## this table describes the cumulative counts (from most frequent to least frequent) for each distinct observation in OBSERVATION_FACT.txt. Step 20 means top 20; step 100 means top 100. Refer to graph for visualization. Skip this line when read in data ##\n");
              writer.write("step,count\n");
              int i = 0;
              for (Integer count : count_hist.stream().collect(Collectors.toList())) {
                  writer.write(String.format("%d,%d\n", i + 20, count));
                  i = i + 20;
              }
-             writer.close();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+
+         //calculate counts by concept categories, e.g. VITAL, LOINC, ICD9, ICD10...
+         String cpt_counts = WORKING_DIR + File.separator + "data" + File.separator + "cpt_counts.csv";
+         Map<String, Integer> cpt_category_count = new HashMap<>();
+         list.forEach(c -> {
+             String cat = c.name.split(":|\\|")[0];
+             if (cpt_category_count.containsKey(cat)) {
+                 cpt_category_count.put(cat, cpt_category_count.get(cat) + c.count);
+             } else {
+                 cpt_category_count.put(cat, c.count);
+             }
+         });
+         String cpt_category_count_path = WORKING_DIR + File.separator + "data" + File.separator + "cpt_category_count.csv";
+
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(cpt_category_count_path))) {
+             writer.write("## this table describes the counts for each observation type (medication, LOINC, vital signs, ICD9, ICD 10 etc) in the OBSERVATION_FACT.txt file. Refer to the graph for visualization. Skip this line when reading in data ##\n");
+             writer.write("category,count\n");
+             cpt_category_count.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                     .forEachOrdered(p -> {
+                 try {
+                     writer.write(p.getKey() + "," + p.getValue() + "\n");
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             });
          } catch (IOException e) {
              e.printStackTrace();
          }
 
     }
+
+    static void cpt_category_count() {
+        String cpt_counts = WORKING_DIR + File.separator + "data" + File.separator + "cpt_counts.csv";
+        Map<String, Integer> cpt_category_count = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(cpt_counts))) {
+            String line;
+            while ((line = reader.readLine()) != null ) {
+                if (line.startsWith("cpt,count")) {
+
+                } else {
+                    String name = line.split(",")[0];
+                    int count = Integer.parseInt(line.split(",")[1]);
+                    String cat = name.split(":|\\|")[0];
+                    if(cpt_category_count.containsKey(cat)) {
+                        cpt_category_count.put(cat, cpt_category_count.get(cat) + count);
+                    } else {
+                        cpt_category_count.put(cat, count);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String cpt_category_count_path = WORKING_DIR + File.separator + "data" + File.separator + "cpt_category_count.csv";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(cpt_category_count_path))) {
+            writer.write("category,count\n");
+            cpt_category_count.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .forEachOrdered(p -> {
+                try {
+                    writer.write(p.getKey() + "," + p.getValue() + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     private static class CPT_Component implements Comparable<CPT_Component> {
 
@@ -173,6 +271,7 @@ public class App {
         final String observation_concept_statistics = "/Users/zhangx/Documents/HUSH+_UNC_JAX/Hush+UNC_JAX/HUSH+/observation_fact_statistics.csv";
 
         concept_freq(observation_concept_statistics);
+        //cpt_category_count();
 
     }
 }
--- src/main/r_script/accumulated_freq_count_obs.R
+++ src/main/r_script/accumulated_freq_count_obs.R
@@ -1,10 +1,18 @@
 library(dplyr)
 library(ggplot2)
 library(scales)
-
+total_count <- 54683532
+cpt_category_counts <- read.csv("/Users/zhangx/git/HushToFhir/data/cpt_category_count.csv", header = TRUE, sep = ",", skip = 1)
+cpt_category_counts$index <- seq(nrow(cpt_category_counts))
+ggplot(cpt_category_counts) + geom_bar(aes(x = index, y = count / total_count), stat = "identity") + 
+  geom_text(aes(x = index, y = count / total_count, label = category, vjust = -2 +1  * (index + 1) %% 2, hjust = (index + 1) %% 2 * 0.4)) +
+  scale_x_continuous(limit = c(0, 15)) + scale_y_continuous(limit = c(0, 0.25), labels = percent) + 
+  xlab("concept category rank") + ylab("percentage frequency") + 
+  theme_bw() + theme(panel.grid = element_blank())
+ggsave("concept_category_freq.pdf", width = 4.5, height = 4)
 ## unfortunately, you need to type in the hard address of the dataset
 ## @TODO: checkout ways to get the directory of current script
-data <- read.csv("/Users/zhangx/git/HushToFhir/data/accumulated_cpt_count.csv", header = TRUE, sep = ",")
+data <- read.csv("/Users/zhangx/git/HushToFhir/data/cumulative_cpt_count.csv", header = TRUE, sep = ",", skip = 1)
 ## total cpt count: 54683532
 ## create a cumulated count graph
 total_count <- 54683532
